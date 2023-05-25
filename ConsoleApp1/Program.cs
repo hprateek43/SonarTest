d2f0a4eeeeef637fee676ce7e80d0d701a3cf2c9
Harsh Singh <harsh.prateek.singh@gmail.com>
2023-05-25 15:51:11 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class ConsoleApp1
+{
+    public static string testfunc()
+    {
+        Console.WriteLine("test");
+        return "hello";
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+}
---
+++ ConsoleApp1Tests/ConsoleApp1Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ConsoleApp1Tests
+    {
+        [TestMethod()]
+        public void testfuncTest()
+        {
+            Assert.AreEqual(ConsoleApp1.testfunc(), "hello");
+        }
+    }
+}
